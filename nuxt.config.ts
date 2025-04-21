e6a2908e43250b8f52cbdab5807c91aeb6df110d
Castelo <jpcbm@ic.ufal.br>
2025-04-20 23:07:34 -0300

    Adiciona configuração de modo de cor na UI do Nuxt, desativando o modo de cor.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/icon"],
   css: ["~/assets/css/main.css"],
+  ui: {
+    colorMode: false,
+  },
 });
