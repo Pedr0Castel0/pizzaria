421318022f0fba1e79f7aaccb2f0727c1ed587f8
Castelo <jpcbm@ic.ufal.br>
2025-04-21 18:35:39 -0300

    Adiciona layout padrão com componentes AppHeader e AppFooter para estruturação da página

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <UApp>
+    <AppHeader />
+    <slot />
+    <AppFooter />
+  </UApp>
+</template>
+
+<script setup>
+import AppHeader from "~/components/AppHeader.vue";
+import AppFooter from "~/components/AppFooter.vue";
+</script>
