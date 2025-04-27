787a49e2ab698c96dd922aced76c43e29022d0b0
Castelo <jpcbm@ic.ufal.br>
2025-04-27 02:55:15 -0300

    Adiciona o módulo @pinia/nuxt ao projeto e configura a variável de ambiente apiBase no runtimeConfig, permitindo a definição da URL da API.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/icon"],
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/icon", "@pinia/nuxt"],
   css: ["~/assets/css/main.css"],
   ui: {
     colorMode: false,
+  },
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:8000",
+    },
   },
 });
