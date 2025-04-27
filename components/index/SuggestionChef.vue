b570b1ba36424101a4ba9472a5e0993103e9f07f
Castelo <jpcbm@ic.ufal.br>
2025-04-27 20:47:18 -0300

    Implementa a lógica de recomendações de pizzas na interface, atualizando a estrutura de dados e permitindo a adição de pizzas ao carrinho com feedback visual. Ajusta a exibição das informações das pizzas e melhora a responsividade do layout.

--- components/index/SuggestionChef.vue
+++ components/index/SuggestionChef.vue
@@ -17,25 +17,25 @@
 
     <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
       <div
-        v-for="pizza in sugestoes"
-        :key="pizza.id"
+        v-for="(pizza, index) in sugestoes"
+        :key="index"
         class="border-2 border-red-500 rounded-lg p-4 flex flex-col shadow bg-white"
       >
         <img
-          :src="pizza.imagem"
+          :src="`/images/pizzas/${pizza.nome}.jpg`"
           :alt="pizza.nome"
-          class="w-full object-cover rounded-lg mb-4"
+          class="w-full h-32 object-cover rounded-lg mb-4"
         />
         <h3 class="font-bold text-base mb-1 text-gray-800">
           {{ pizza.nome }}
         </h3>
-        <p class="text-gray-600 text-sm mb-4">{{ pizza.descricao }}</p>
         <div class="flex items-center justify-between mt-auto">
           <span class="text-red-600 font-bold text-lg"
             >R$ {{ pizza.preco }}</span
           >
           <button
             class="text-red-600 border border-red-500 px-4 py-1 rounded hover:bg-red-100 text-sm font-medium"
+            @click="addPizzaToCart(pizza)"
           >
             Pedir agora
           </button>
@@ -46,28 +46,108 @@
 </template>
 
 <script setup lang="ts">
-const sugestoes = [
+import { useRecommendations } from "~/composables/useRecommendations";
+import { useCartStore } from "~/stores/cart";
+const toast = useToast();
+
+interface PizzaRecomendada {
+  nome: string;
+  preco: number;
+}
+
+const sugestoes = ref<PizzaRecomendada[]>([]);
+
+const { obterRecomendacoes } = useRecommendations();
+
+onMounted(async () => {
+  const recomendacoes = await obterRecomendacoes();
+  sugestoes.value = recomendacoes?.pizzas_recomendadas ?? [];
+  console.log("recomendacoes", sugestoes.value);
+});
+
+const addPizzaToCart = (pizza: PizzaRecomendada) => {
+  const cartStore = useCartStore();
+  const pizzaEncontrada = mockPizzas.find(
+    (p) => p.name.toLowerCase() === pizza.nome.toLowerCase()
+  );
+
+  if (pizzaEncontrada) {
+    cartStore.add({
+      id: pizzaEncontrada.id,
+      name: pizzaEncontrada.name,
+      description: pizzaEncontrada.description,
+      price: pizzaEncontrada.price,
+      quantity: 1,
+      category: pizzaEncontrada.category,
+      image: pizzaEncontrada.image,
+      size: pizzaEncontrada.size,
+    });
+
+    toast.add({
+      title: "Pizza adicionada ao carrinho",
+      description: "Pizza adicionada ao carrinho com sucesso",
+      icon: "i-heroicons-check-circle",
+      color: "success",
+      duration: 2000,
+    });
+  }
+};
+
+const mockPizzas = [
   {
     id: 1,
-    nome: "Margherita",
-    descricao:
-      "Molho de tomate, Mussarela de búfala, Manjericão fresco e Azeite de oliva",
-    preco: "69.90",
-    imagem: "/images/pizzas/margherita.jpg",
+    name: "Margherita",
+    description: "Molho de tomate, mussarela, manjericão fresco e azeite",
+    price: 45.0,
+    size: "8 fatias",
+    category: "Pizzas Tradicionais",
+    image: "/images/pizzas/margherita.jpg",
   },
   {
     id: 2,
-    nome: "Cinco Queijos",
-    descricao: "Mussarela, Gorgonzola, Provolone, Parmesão e Catupiry",
-    preco: "69.90",
-    imagem: "/images/pizzas/cinco-queijos.jpg",
+    name: "Pepperoni",
+    description: "Molho de tomate, mussarela e pepperoni",
+    price: 55.0,
+    size: "8 fatias",
+    category: "Pizzas Tradicionais",
+    image: "/images/pizzas/pepperoni.jpg",
   },
   {
     id: 3,
-    nome: "Queijo Brie Picante",
-    descricao: "Queijo Brie, Mel e Pimenta",
-    preco: "79.00",
-    imagem: "/images/pizzas/brie-picante.jpg",
+    name: "Quatro Queijos",
+    description: "Molho de tomate, mussarela, parmesão, gorgonzola e catupiry",
+    price: 60.0,
+    size: "8 fatias",
+    category: "Pizzas Tradicionais",
+    image: "/images/pizzas/quatro-queijos.jpg",
+  },
+  {
+    id: 4,
+    name: "Frango com Catupiry",
+    description: "Molho de tomate, mussarela, frango desfiado e catupiry",
+    price: 55.0,
+    size: "8 fatias",
+    category: "Pizzas Tradicionais",
+    image: "/images/pizzas/frango-catupiry.jpg",
+  },
+  {
+    id: 5,
+    name: "Calabresa",
+    description: "Molho de tomate, mussarela, calabresa fatiada e cebola",
+    price: 50.0,
+    size: "8 fatias",
+    category: "Pizzas Tradicionais",
+    image: "/images/pizzas/calabresa.jpg",
+  },
+  {
+    id: 6,
+    name: "Vegetariana",
+    description:
+      "Molho de tomate, mussarela, tomate, cebola, pimentão, champignon e azeitonas",
+    price: 55.0,
+    size: "8 fatias",
+    category: "Pizzas Tradicionais",
+    image: "/images/pizzas/vegetariana.jpg",
   },
 ];
 </script>
