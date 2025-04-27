82f284da7b5c84ef103882d3f10dc05ac27e3f5d
Castelo <jpcbm@ic.ufal.br>
2025-04-27 19:22:05 -0300

    Adiciona funcionalidades de persistência para o ID do usuário na store, incluindo armazenamento e remoção do ID no localStorage, além de inicialização do estado do usuário ao carregar a aplicação.

--- stores/user.ts
+++ stores/user.ts
@@ -9,10 +9,26 @@ export const useUserStore = defineStore("user", {
     setUser(id: number) {
       this.usuarioId = id;
       this.isLoggedIn = true;
+      if (import.meta.client) {
+        localStorage.setItem("usuario_id", JSON.stringify(id));
+      }
     },
     logout() {
       this.usuarioId = null;
       this.isLoggedIn = false;
+      if (import.meta.client) {
+        localStorage.removeItem("usuario_id");
+      }
+    },
+    init() {
+      if (import.meta.client) {
+        console.log("init");
+        const usuarioId = localStorage.getItem("usuario_id");
+        if (usuarioId) {
+          this.usuarioId = JSON.parse(usuarioId);
+          this.isLoggedIn = true;
+        }
+      }
     },
   },
 });
