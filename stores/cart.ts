7f46d48c791907b12a9811903dc52ba1f9171ba7
Castelo <jpcbm@ic.ufal.br>
2025-04-27 15:52:00 -0300

    Adiciona a store de carrinho utilizando Pinia, com estado para gerenciar itens de pizza, incluindo ações para adicionar, remover e limpar itens do carrinho, além de um getter para calcular o total.

---
+++ stores/cart.ts
@@ -0,0 +1,38 @@
+import { defineStore } from "pinia";
+
+interface Pizza {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  quantity: number;
+  image: string;
+  size: string;
+  info?: string;
+}
+
+export const useCartStore = defineStore("cart", {
+  state: () => ({
+    items: [] as Pizza[],
+  }),
+  getters: {
+    total: (state) => state.items.reduce((sum, pizza) => sum + pizza.price, 0),
+  },
+  actions: {
+    add(pizza: Pizza) {
+      const existingPizza = this.items.find((item) => item.id === pizza.id);
+      if (existingPizza) {
+        existingPizza.quantity += 1;
+      } else {
+        this.items.push({ ...pizza, quantity: 1 });
+      }
+    },
+    remove(idx: number) {
+      this.items.splice(idx, 1);
+    },
+    clear() {
+      this.items = [];
+    },
+  },
+});
