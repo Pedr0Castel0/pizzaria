3dd51e191fc6c46c9348dc8a7959c06f9f03199a
Castelo <jpcbm@ic.ufal.br>
2025-05-05 12:19:54 -0300

    Atualiza a configuração do Nuxt para alterar a base da API, mudando de localhost para a URL de produção da aplicação.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,8 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:8000",
+      apiBase:
+        process.env.NUXT_PUBLIC_API_BASE || "https://back-pizzaria.vercel.app",
     },
   },
 });
